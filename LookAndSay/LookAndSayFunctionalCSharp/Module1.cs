49170728449ed62cd967de7f45dbdd76c0583fc8
Yacoub Massad <yacoubmassad@gmail.com>
2021-03-03 20:04:33 +0200

    Adding the functional C# project

---
+++ LookAndSay/LookAndSayFunctionalCSharp/Module1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using MoreLinq;
+
+namespace LookAndSayFunctionalCSharp
+{
+    public static class Module1
+    {
+        record Group (char Digit, int Count)
+        {
+            public string Encode() => Count.ToString() +  Digit;
+        }
+
+        public static string LookAndSay(string str)
+        {
+            var stringDigits = str.ToImmutableArray();
+
+            Group IncrementCount(Group group) => group with { Count = group.Count + 1 };
+
+            Group GetOneGroup (ImmutableArray<char> nonEmptyDigits) =>
+                nonEmptyDigits switch
+                {
+                    { Length: >= 2} when nonEmptyDigits[0] == nonEmptyDigits[1] =>
+                        IncrementCount(
+                            GetOneGroup(nonEmptyDigits.Skip(1).ToImmutableArray())),
+                    _ => new Group(nonEmptyDigits[0], 1)
+                };
+            
+            (Group group, ImmutableArray<char> remainingDigits)? GetOneGroupAndRemainingDigits(ImmutableArray<char> digits)
+            {
+                if (digits.IsEmpty)
+                    return null;
+
+                var group = GetOneGroup(digits);
+
+                return (group, digits.Skip(group.Count).ToImmutableArray());
+            }
+
+            var groups = MoreEnumerable.Unfold(
+                state: stringDigits,
+                generator: GetOneGroupAndRemainingDigits,
+                predicate: x => x.HasValue,
+                stateSelector: x => x.Value.remainingDigits,
+                resultSelector: x => x.Value.group);
+
+            return string.Concat(groups.Select(x => x.Encode()));
+        }
+    }
+}
--- LookAndSay/Tests/Tests.cs
+++ LookAndSay/Tests/Tests.cs
@@ -2,25 +2,35 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
 {
-    public enum Language { CSharp, FSharp }
+    public enum Language { CSharp, FSharp, FunctionalCSharp }
 
     [TestClass]
     public class Tests
     {
         [DataTestMethod]
+        [DataRow("", "", Language.CSharp)]
+        [DataRow("", "", Language.FSharp)]
+        [DataRow("", "", Language.FunctionalCSharp)]
         [DataRow("1", "11", Language.CSharp)]
         [DataRow("1", "11", Language.FSharp)]
+        [DataRow("1", "11", Language.FunctionalCSharp)]
         [DataRow("33", "23", Language.CSharp)]
         [DataRow("33", "23", Language.FSharp)]
+        [DataRow("33", "23", Language.FunctionalCSharp)]
         [DataRow("11112222", "4142", Language.CSharp)]
         [DataRow("11112222", "4142", Language.FSharp)]
+        [DataRow("11112222", "4142", Language.FunctionalCSharp)]
         [DataRow("1112334", "31122314", Language.CSharp)]
         [DataRow("1112334", "31122314", Language.FSharp)]
+        [DataRow("1112334", "31122314", Language.FunctionalCSharp)]
         public void Test(string input, string expectedOutput, Language language)
         {
-            var output = language == Language.CSharp
-                ? LookAndSayCSharp.Module1.LookAndSay(input)
-                : LookAndSayFSharp.Module1.lookAndSay(input);
+            var output = language switch
+            {
+                Language.CSharp => LookAndSayCSharp.Module1.LookAndSay(input),
+                Language.FSharp => LookAndSayFSharp.Module1.lookAndSay(input),
+                Language.FunctionalCSharp => LookAndSayFunctionalCSharp.Module1.LookAndSay(input),
+            };
             
             Assert.AreEqual(expectedOutput, output);
         }
